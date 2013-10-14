b076dfc11fdf18b537be26f8e8b2752eba5cbf0c
Daniel Felipe González Obando <danyfel80@gmail.com>
2013-10-14 15:57:02 -0500

    HomeActivity, RegisterActivity, ReminderSetupActivity UI

--- src/com/banlinea/control/HomeActivity.java
+++ src/com/banlinea/control/HomeActivity.java
@@ -1,15 +1,47 @@
 package com.banlinea.control;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class HomeActivity extends Activity {
+	
+	private EditText eMailTextView;
+	private EditText passwordTextView;
+	private Button registerButton;
+	private Button signInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        
+        eMailTextView = (EditText) findViewById(R.id.email_edittext);
+        passwordTextView = (EditText) findViewById(R.id.password_edittext);
+        
+        registerButton = (Button) findViewById(R.id.register_button);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(HomeActivity.this, RegisterActivity.class);
+				startActivity(intent);
+			}
+		});
+        
+        signInButton = (Button) findViewById(R.id.sign_in_button);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
     }
 
 
---
+++ src/com/banlinea/control/RegisterActivity.java
@@ -0,0 +1,84 @@
+package com.banlinea.control;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class RegisterActivity extends Activity {
+	
+	private EditText firstNameTextView;
+	private EditText lastNameTextView;
+	private EditText eMailTextView;
+	private EditText passwordTextView;
+	private EditText passwordConfirmTextView;
+	
+	private Button registerButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        // Show the Up button in the action bar.
+        setupActionBar();
+        
+        firstNameTextView = (EditText) findViewById(R.id.first_name_edittext);
+        lastNameTextView = (EditText) findViewById(R.id.last_name_edittext);
+        eMailTextView = (EditText) findViewById(R.id.email_edittext);
+        passwordTextView = (EditText) findViewById(R.id.password_edittext);
+        passwordConfirmTextView = (EditText) findViewById(R.id.password_confirm_edittext);
+        
+        registerButton = (Button) findViewById(R.id.register_button);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(null, ReminderSetup.class);
+				startActivity(intent);
+			}
+		});
+    }
+
+    /**
+     * Set up the {@link android.app.ActionBar}, if the API is available.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    private void setupActionBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.register, menu);
+        return true;
+    }
+    
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // This ID represents the Home or Up button. In the case of this
+                // activity, the Up button is shown. Use NavUtils to allow users
+                // to navigate up one level in the application structure. For
+                // more details, see the Navigation pattern on Android Design:
+                //
+                // http://developer.android.com/design/patterns/navigation.html#up-vs-back
+                //
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
