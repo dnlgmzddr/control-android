64db0b3a0b01e9f2e344244dc8bc4e0497016d40
Daniel Gomez Didier <dnlgmzddr@gmail.com>
2013-10-20 22:31:14 -0500

    Base service.

---
+++ src/com/banlinea/control/bussiness/BaseService.java
@@ -0,0 +1,24 @@
+package com.banlinea.control.bussiness;
+
+import android.content.Context;
+
+import com.banlinea.control.local.DatabaseHelper;
+import com.j256.ormlite.android.apptools.OpenHelperManager;
+
+public class BaseService {
+	private DatabaseHelper databaseHelper = null;
+
+	private Context context;
+	
+	public BaseService(Context context){
+		this.context = context;
+	}
+	
+	protected DatabaseHelper getHelper() {
+		if (databaseHelper == null) {
+			databaseHelper = OpenHelperManager.getHelper(this.context,
+					DatabaseHelper.class);
+		}
+		return databaseHelper;
+	}
+}
