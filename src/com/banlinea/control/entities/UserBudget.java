dd47ac13397fb8ffebe41ceb67263a58903ff40d
Daniel Gomez Didier <dnlgmzddr@gmail.com>
2013-11-10 20:51:26 -0500

    Mode isFixed to budget.

--- src/com/banlinea/control/entities/UserBudget.java
+++ src/com/banlinea/control/entities/UserBudget.java
@@ -7,61 +7,75 @@ import com.j256.ormlite.table.DatabaseTable;
 
 @DatabaseTable
 public class UserBudget implements Serializable {
-	
+
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -3638660564955995096L;
-	
-	
+
 	@DatabaseField(id = true)
 	private String idCategory;
 	@DatabaseField(index = true)
 	private String idUser;
 	@DatabaseField
 	private float budget;
-	
+	@DatabaseField
+	private boolean isFixed;
+
 	/**
 	 * Indicate if the period of the budget is Monthly, Weekly... etc.
 	 */
 	@DatabaseField
 	private int period;
-	
+
 	private float currentExecutedBudget;
-	
+
 	public String getIdCategory() {
 		return idCategory;
 	}
+
 	public void setIdCategory(String idCategory) {
 		this.idCategory = idCategory;
 	}
+
 	public String getIdUser() {
 		return idUser;
 	}
+
 	public void setIdUser(String idUser) {
 		this.idUser = idUser;
 	}
+
 	public float getBudget() {
 		return budget;
 	}
+
 	public void setBudget(float budget) {
 		this.budget = budget;
 	}
+
 	public int getPeriod() {
 		return period;
 	}
+
 	public void setPeriod(int period) {
 		this.period = period;
 	}
-	
+
 	public float getCurrentExecutedBudget() {
 		return currentExecutedBudget;
 	}
+
 	public void setCurrentExecutedBudget(float currentExecutedBudget) {
 		this.currentExecutedBudget = currentExecutedBudget;
 	}
-	
-	
-	
-	
+
+	public boolean isFixed() {
+		return isFixed;
+	}
+
+	public void setFixed(boolean isFixed) {
+		this.isFixed = isFixed;
+	}
+
 }
